0ba5a93c6c06937f5b4351a9bc3086d5e44545a7
Hennus Bergman <hennus.bergman@gmail.com>
2015-08-22 11:22:03 +0200

    Use statemachine so that LED behaviour is easier to program. Turn servo off when idle.

--- cat-feeder-3cup.ino
+++ cat-feeder-3cup.ino
@@ -1,19 +1,18 @@
 /*
  * Uses a micro-servo with 3 tubes to drop kibble from a tube through a hole.
  * 
- * - servo
- * - Digital input button for 'feed-me-now', for testing
- * - LED output 
- * 
  * Wakes up every second to check the time and goes back to sleep if nothing
  * to be done. This should save battery power.
  * 
  * Copyright (c) by Hennus Bergman
  * 
+ * Red LED on pin 11 with resistor 300 ohm
+ * Green LED on pin 10 with resistor 300 ohm
+ * Servo on pin 9
  * 
+ * TODO:
  * pushbutton attached to pin 2 from +5V
  * 10K resistor attached to pin 2 from ground
- * 
  * 
  */
 #include <Servo.h>
@@ -30,17 +29,34 @@ const int FEED_POS_OPEN1 = 53;
 const int FEED_POS_OPEN2 = 105;
 const int FEED_POS_OPEN3 = 165;
 
+const long DELAY_2_AND_A_HALF_HOURS_IN_MILLIS = (5L * 60L * 60L * 1000L) / 2L;
+// const long DELAY_2_AND_A_HALF_HOURS_IN_MILLIS = 30000L; // for testing
+const long DELAY_CUP1 = 1L * DELAY_2_AND_A_HALF_HOURS_IN_MILLIS;
+const long DELAY_CUP2 = 2L * DELAY_2_AND_A_HALF_HOURS_IN_MILLIS;
+const long DELAY_CUP3 = 3L * DELAY_2_AND_A_HALF_HOURS_IN_MILLIS;
+
+enum feeder_state {
+  STATE_RESET,
+  STATE_CUP1,
+  STATE_CUP2,
+  STATE_CUP3,
+  STATE_DONE
+};
+
+enum feeder_state state = STATE_RESET;
+
 Servo myservo;  // create servo object to control a servo
-
-
 
 void setup() {
   Serial.begin(9600);
-  // servo setup - use custom settings to get a little over 180 degrees
-  myservo.attach(SERVO_PIN, 425, 2500);  // attaches the servo on pin 9 to the servo object
-  delay(100);
+  Serial.print("  DELAY_2_AND_A_HALF_HOURS_IN_MILLIS=");
+  Serial.println(DELAY_2_AND_A_HALF_HOURS_IN_MILLIS);
+
+  // position servo at reset to closed position.
+  attach_servo();
   // servo_move(1, FEED_POS_CLOSED, 1, 15);
   myservo.write(FEED_POS_CLOSED);
+  detach_servo();
 
   // initialize the LED pins as output:
   pinMode(GREEN_LED_PIN, OUTPUT);
@@ -50,61 +66,125 @@ void setup() {
   // initialize the pushbutton pin as an input:
   //  pinMode(BUTTON_PIN, INPUT);
 
-  // disable the onboard LED
+  // disable the onboard LED to save power
   digitalWrite(ONBOARD_LED_PIN, LOW);
 
-
-  delay(5000);
-  Serial.println("1");
-  feed_cup1();
-  delay(5000);
-  Serial.println("2");
-  feed_cup2();
-  delay(5000);
-  Serial.println("3");
-  feed_cup3();
-  delay(5000);
-  Serial.println("close");
-  feed_close();
+  digitalWrite(GREEN_LED_PIN, HIGH);
+  digitalWrite(RED_LED_PIN, LOW);
 }
 
 int redVal = 0;
-int ledPhase = 1;
+int ledPhase = 25;
+
+
+
 void loop() {
-//  Serial.println(redVal);
-//  analogWrite(RED_LED_PIN, redVal);
-  redVal += ledPhase;
-  if (redVal > 255 || redVal < 0) {
-    ledPhase = -1 * ledPhase;
+  switch (state) {
+    case STATE_RESET: {
+      // wait for CUP1 time
+      if (millis() > DELAY_CUP1) {
+        state = STATE_CUP1;
+        Serial.println("cup 1");
+        feed_cup1();
+      }
+      break;
+    }
+    case STATE_CUP1: {
+      // wait for CUP2 time
+      if (millis() > DELAY_CUP2) {
+        state = STATE_CUP2;
+        Serial.println("cup 2");
+        feed_cup2();
+      } else {
+        blink_green(1);
+      }
+      break;
+    }
+    case STATE_CUP2: {
+      // wait for CUP3 time
+      if (millis() > DELAY_CUP3) {
+        state = STATE_CUP3;
+        Serial.println("cup 3");
+        feed_cup3();
+      } else {
+        blink_green(2);
+      }
+      break;
+    }
+    case STATE_CUP3: {
+      state = STATE_DONE;
+      // done; turn the green light off.
+      digitalWrite(GREEN_LED_PIN, LOW);
+      Serial.println("done");
+      feed_close();
+      break;
+    }
+
+    case STATE_DONE: {
+      // maintain RED light
+      analogWrite(RED_LED_PIN, redVal);
+      redVal += ledPhase;
+      if (redVal >= 255 || redVal <= 0) {
+        ledPhase = -1 * ledPhase;
+      }
+      delay(100);
+      break;
+    }
+
   }
-  delay(100);
+  
+  // sleep one second when not needing to maintain the RED light status
+  if (state != STATE_DONE) {
+    delay(1000);
+  }
 }
 
-
-
-void loopX() {
+void blink_green(int numTimes) {
+  delay(1000);
+  for (int i=0; i < numTimes; i++) {
+    delay(300);
+    digitalWrite(GREEN_LED_PIN, LOW);
+    delay(200);
+    digitalWrite(GREEN_LED_PIN, HIGH);
+    delay(300);
+  }
   delay(1000);
 }
 
 
+void attach_servo() {
+  // servo setup - use custom settings to get a little over 180 degrees
+  myservo.attach(SERVO_PIN, 425, 2500);  // attaches the servo on pin 9 to the servo object
+  delay(100);
+}
 
+void detach_servo() {
+  // release servo control to keep the servo engine quiet.
+  myservo.detach();
+}
 
 void feed_cup1() {
+  attach_servo();
   servo_move(FEED_POS_CLOSED, FEED_POS_OPEN1, 1, 15);
+  detach_servo();
 }
 
 void feed_cup2() {
+  attach_servo();
   servo_move(FEED_POS_OPEN1, FEED_POS_OPEN2, 1, 15);
+  detach_servo();
 }
 
 void feed_cup3() {
+  attach_servo();
   servo_move(FEED_POS_OPEN2, FEED_POS_OPEN3, 1, 15);
-  delay(500);
-  servo_move(FEED_POS_OPEN3, FEED_POS_OPEN3 + 3, 1, 15);
+  detach_servo();
 }
 
 void feed_close() {
+  attach_servo();
   servo_move(FEED_POS_OPEN3, FEED_POS_CLOSED, 1, 15);
+  detach_servo();
 }
 
 void servo_move(int from, int to, int stepSize, int stepDelay) {
@@ -142,6 +222,5 @@ void servo_move(int from, int to, int stepSize, int stepDelay) {
 
   myservo.write(to);
   delay(stepDelay);
-
 }
 
